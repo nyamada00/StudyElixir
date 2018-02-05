22e86d1264a8fbb49b97f52d8cebee252167f34f
nyamada <nyamada@okaya-system.co.jp>
2018-02-05 13:13:55 +0900

    Chapter11

---
+++ Chapter11/OrdersCsv.exs
@@ -0,0 +1,32 @@
+defmodule OrdersCsv do
+    def parse(filename) do
+        {:ok,orders}=File.open(filename,[:read],fn(file)->
+            headers = IO.read(file,:line) |> parse_headers
+            Enum.map IO.stream(file,:line), &(parse_order(&1,headers))
+        end)
+        orders
+    end
+    def parse_headers(line) do
+        line
+        |> split_columns
+        |> Enum.map(&String.to_atom/1)
+    end
+    def parse_order(line,headers) do
+        order=line
+            |>split_columns
+            |>convert_types
+        Enum.zip headers,order
+    end
+    defp split_columns(line) do
+        line
+        |>String.trim
+        |>String.split(",")
+    end
+    defp convert_types([id,":"<>city,net_amount]) do
+        [
+            String.to_integer(id),
+            String.to_atom(city),
+            String.to_float(net_amount)
+        ]
+    end
+end
